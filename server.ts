74f491fc2df2ba124a5b086173831640ecaaf00d
andrewijaya <andrewijaya988@gmail.com>
2023-12-20 09:19:45 +0700

    update: use logger

--- server.ts
+++ server.ts
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
 import app from "./src/index";
+import logger from "./src/utils/logger";
 
 dotenv.config();
 const port = process.env.port || 9999;
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  logger.info(`Server is running at http://localhost:${port}`);
 });
